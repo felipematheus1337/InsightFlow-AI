45e611117b4f27eddc87bbb609f82f94e1e3f086
felipematheus1337 <lipehbr@gmail.com>
2026-06-04 17:53:45 -0300

    Criado router.

---
+++ go-crud-ms/router/routes.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/felipematheus1337/InsightFlow-AI/go-crud-ms/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(router *gin.Engine, e *handler.EmployeeHandler) {
+
+	basePath := "/api/v1/employees"
+
+	v1 := router.Group(basePath)
+
+	RegisterEmployeeRoutes(v1, e)
+}
+
+func RegisterEmployeeRoutes(v1 *gin.RouterGroup, e *handler.EmployeeHandler) {
+
+	{
+		v1.POST("/", e.CreateEmployee)
+	}
+}
