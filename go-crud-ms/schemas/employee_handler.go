83f9290d53d0a3a8660855cc4893d48be2374f1a
felipematheus1337 <lipehbr@gmail.com>
2026-06-04 17:32:17 -0300

    Criado handler, e método para criação de employee

---
+++ go-crud-ms/schemas/employee_handler.go
@@ -0,0 +1,47 @@
+package schemas
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type EmployeeHandler struct {
+	service *service.EmployeeService
+}
+
+func NewEmployeeHandler(service *service.EmployeeService) *EmployeeHandler {
+	return &EmployeeHandler{service}
+}
+
+func (h *EmployeeHandler) CreateEmployee(ctx *gin.Context) {
+
+	var employeeDTO dto.CreateEmployeeDTO
+
+	if err := ctx.ShouldBind(&employeeDTO); err != nil {
+		sendError(ctx http.StatusBadRequest, err.Error())
+		return
+	}
+
+	errValidate := employeeDTO.Validate()
+
+	if errValidate != nil {
+		msgErro := errValidate.Error()
+		errStrings := strings.Split(msgErro, ",")
+		ErrParamIsRequired(errStrings[0], errStrings[1])
+	}
+
+	employee := mapper.CreateToSchema(employeeDTO)
+
+    response, err := p.service.CreateEmployee(employee)
+
+   if err != nil {
+	   sendError(ctx, http.StatusInternalServerError, err.Error())
+	   return
+   }
+
+   sendSuccess(ctx, "create-employee", response, http.StatusCreated)
+
+
+}
